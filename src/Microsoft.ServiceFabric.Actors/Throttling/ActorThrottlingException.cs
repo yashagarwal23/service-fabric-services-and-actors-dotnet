9632d34a8e773fc2fd3d86f8a0777143f230f81a
Yash Agarwal <yash23.agarwal@gmail.com>
2022-01-03 11:34:50 +0530

    added other constructors in ActorThrottlingException

--- src/Microsoft.ServiceFabric.Actors/Throttling/ActorThrottlingException.cs
+++ src/Microsoft.ServiceFabric.Actors/Throttling/ActorThrottlingException.cs
@@ -7,6 +7,7 @@ namespace Microsoft.ServiceFabric.Actors.Throttling
 {
     using System;
     using System.Fabric;
+    using System.Runtime.Serialization;
 
     /// <summary>
     /// Exception thrown when actor throttler rejects a call.
@@ -17,9 +18,38 @@ namespace Microsoft.ServiceFabric.Actors.Throttling
         /// <summary>
         /// Initializes a new instance of the <see cref="ActorThrottlingException"/> class.
         /// </summary>
+        public ActorThrottlingException()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ActorThrottlingException"/> class.
+        /// </summary>
         /// <param name="message">The error message that explains the reason for the exception.</param>
         public ActorThrottlingException(string message)
             : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ActorThrottlingException"/> class with a specified error message and
+        /// a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="inner">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
+        public ActorThrottlingException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ActorThrottlingException"/> class with serialized data.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> that holds the serialized object data about the exception being thrown. </param>
+        /// <param name="context">The <see cref="StreamingContext"/> that contains contextual information about the source or destination.</param>
+        private ActorThrottlingException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
         }
     }
